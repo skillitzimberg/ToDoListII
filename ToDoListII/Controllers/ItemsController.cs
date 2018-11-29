206dee62d949d0c59f071711296410b3e5ce6dee
Scott Bergler and Tavish OKeefe <scott+tavish@>
2018-11-28 20:16:57 -0800

    add homework updates for 11/28.

--- ToDoListII/Controllers/ItemsController.cs
+++ ToDoListII/Controllers/ItemsController.cs
@@ -4,41 +4,32 @@ using ToDoListII.Models;
 
 namespace ToDoListII.Controllers
 {
-    public class ItemsController : Controller
+  public class ItemsController : Controller
+  {
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-          List<Item> allItems = Item.GetAll();
-          return View(allItems);
-        }
-
-        [HttpGet("/items/new")]
-        public ActionResult New()
-        {
-          return View();
-        }
-
-        [HttpPost("/items")]
-        public ActionResult Create(string description)
-        {
-          Item newTask = new Item(description);
-          return RedirectToAction("Index");
-        }
-
-        [HttpGet("/items/{id}")]
-        public ActionResult Show(int id)
-        {
-          Item item = Item.Find(id);
-          return View(item);
-        }
-
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
-        {
-          Item.ClearAll();
-          return View();
-        }
-
+       Category category = Category.Find(categoryId);
+       return View(category);
     }
+
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category category = Category.Find(categoryId);
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+  }
 }
